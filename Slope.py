a2897af53a3d1df541ee7a92e3c14af1c8d8b84e
uarif <uarif@users.noreply.github.com>
2023-09-01 12:40:48 -0400

    Add files via upload

---
+++ Slope.py
@@ -0,0 +1,17 @@
+def getslope (x0,y0,x1,y1):
+num= y1-y0
+den= x1-x0
+if abs (den)>0.0001:
+	slope=num/den
+else:
+Print 'Invalid input'
+slope=num
+return slope
+#unit tests
+print 'unit test 1'
+x0 =0.0
+y0 = 0.0
+x1 = 0.0
+y1 = 1.0
+slope = getslope (x0,y0,x1,y1)
+print slope
